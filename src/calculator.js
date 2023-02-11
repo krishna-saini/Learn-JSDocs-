43de48a80cf6fe3e249a4dcbf1016f44d6be6d2b
krishna-saini <krishnapraanjal10@gmail.com>
2023-02-11 10:44:47 +0530

    jsdoc for modules add

---
+++ src/calculator.js
@@ -0,0 +1,21 @@
+/**
+ * Calculator module - See {@tutorial calculator-tutorial}
+ * @module calculator
+ */
+
+/**
+ *Add two numbers
+ * @param {number} n1 - First number
+ * @param {number} n2 - Second number
+ * @returns {number} - add of n1 & n2
+ */
+
+exports.add = (n1, n2) => n1 + n2;
+
+/**
+ * Subtract numbers
+ * @param {number} n1 - First number
+ * @param {number} n2 - Second number
+ * @returns {number} - subtraction of n1 & n2
+ */
+exports.subtract = (n1, n2) => n1 - n2;
